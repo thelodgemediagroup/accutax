f52532c6008d52f8c8fa6d50a4e8057e4eb73f3d
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-11-01 16:12:33 -0400

    Page layout, hero setup

--- dev/application/views/home.php
+++ dev/application/views/home.php
@@ -1,3 +1,9 @@
 	<div class="home container">
-		<h1>Welcome to Accutax</h1>
+		<div class="hero container">
+			<h1 style="font-size:3em;padding: 170px 0px 0px 360px;color: #FFF;">Hero Slider???<br>CSS Animations?</h1>
+		</div><!--/ .hero -->
+
+		<div class="featured container">
+			
+		</div><!--/ .featured -->
 	</div><!--/ .home -->
