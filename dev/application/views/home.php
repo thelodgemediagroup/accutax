32e08a3e7f102f04767c77e2c9e3fb87a02ca6ed
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-11-02 19:09:32 -0400

    Home page: start filling/styling

--- dev/application/views/home.php
+++ dev/application/views/home.php
@@ -1,9 +1,23 @@
 	<div class="home container">
-		<div class="hero container">
+		<div class="hero container clear">
 			<h1 style="font-size:3em;padding: 170px 0px 0px 360px;color: #FFF;">Hero Slider???<br>CSS Animations?</h1>
 		</div><!--/ .hero -->
 
-		<div class="featured container">
-			
+		<div class="featured container clear">
+			<div class="featured-el colspan-three floatleft">
+				<h2 class="bold text-center">Business</h2>
+				<h3>Own Your Business</h3>
+				<p>Let us manage your finances. We offer Bookkeeping, Income Tax Returns, Payroll, Quickbooks help</p>
+			</div><!--/ .colspan-three -->
+			<div class="featured-el colspan-three floatleft">
+				<h2 class="bold text-center">Personal</h2>
+				<h3>Get back the money you deserve</h3>
+				<p>Our thorough approach earns you more money even with complicated returns that others won't touch</p>
+			</div><!--/ .colspan-three -->
+			<div class="featured-el colspan-three floatleft">
+				<h2 class="bold text-center">Quickbooks</h2>
+				<h3>Talk to Our Pros</h3>
+				<p>We know Quickbooks inside and out. We can help you become a pro too. Find out more</p>
+			</div><!--/ .colspan-three -->						
 		</div><!--/ .featured -->
 	</div><!--/ .home -->
