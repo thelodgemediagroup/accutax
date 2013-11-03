4a513b3088a7fccaf7325de8067235e8d8b0503c
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-11-03 16:37:15 -0500

    Footer style, finishing nav - unfilled dropdown links

--- dev/application/views/footer.php
+++ dev/application/views/footer.php
@@ -1,5 +1,29 @@
 		<div class="footer wrapper">
-			<div class="inner-footer container">
+			<div class="inner-footer wrapper">
+				<div class="colspan-four footer-col">
+
+				</div><!--/ colspan-four -->
+				<div class="colspan-four footer-col">
+				</div><!--/ colspan-four -->
+				<div class="colspan-four footer-col">
+				</div><!--/ colspan-four -->
+				<div class="colspan-four footer-col">
+					<h2>Contact Us</h2>
+					<p class="line-accent">
+						<i class="fa fa-phone"></i>
+						&nbsp; (586) 580-3040
+					</p>
+					<p class="line-accent">
+						<i class="fa fa-envelope"></i>
+						&nbsp; <a href="">info@eaccutax.com</a>
+					</p>
+					<p>&nbsp;</p>
+					<p>Accutax</p>
+					<p>8639 Hall Rd</p>
+					<p>Utica, MI 48317</p>
+					<p><a href="">Map</p>
+				</div><!--/ colspan-four -->
+
 			</div><!--/ .inner-footer -->
 		</div><!--/ .footer -->
 	</div><!--/ .wrapper -->
--- dev/application/views/header.php
+++ dev/application/views/header.php
@@ -4,6 +4,7 @@
 	<title>Accutax</title>
 	<link rel="stylesheet" type="text/css" href="<?php echo get_asset('css/style.css'); ?>">
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,400italic' rel='stylesheet' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.css" rel="stylesheet">
 </head>
 <body>
 	<div class="wrapper">
@@ -11,8 +12,8 @@
 			<div class="inner-header container">
 				<img src="<?php echo get_asset('images/accutax_logo.png'); ?>" alt="Accutax Accounting" />
 				<ul class="main-nav floatright">
-					<li>
-						<a href="">Business</a>
+					<li class="dropdown">
+						<a href="">Business <i class="fa fa-angle-down"></i></a>
 						<ul class="drop-down">
 							<li><a href="">Bookkeeping</a></li>
 							<li><a href="">Payroll</a></li>
@@ -21,8 +22,8 @@
 							<li><a href="">IRS Help</a></li>
 						</ul>
 					</li>
-					<li>
-						<a href="">Personal</a>
+					<li class="dropdown">
+						<a href="">Personal <i class="fa fa-angle-down"></i></a>
 						<ul class="drop-down">
 							<li><a href="">Income Tax Returns</a></li>
 							<li><a href="">Quickbooks</a></li>
