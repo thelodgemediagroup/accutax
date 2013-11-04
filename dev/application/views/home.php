63f489b4a9fba5ab5ed77a51aa134f9d8204e070
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-11-04 00:02:13 -0500

    Resources page

--- dev/application/views/home.php
+++ dev/application/views/home.php
@@ -6,17 +6,17 @@
 		<div class="featured container clear">
 			<div class="featured-el colspan-three">
 				<h2 class="bold text-center">Business</h2>
-				<h3 class="text-center emphasis"><span class="italic">Own</span> Your Business</h3>
+				<h3 class="text-center emphasis accent"><span class="italic">Own</span> Your Business</h3>
 				<p>Let us manage your finances. We offer Bookkeeping, Income Tax Returns, Payroll, Quickbooks help</p>
 			</div><!--/ .colspan-three -->
 			<div class="featured-el colspan-three">
 				<h2 class="bold text-center">Personal</h2>
-				<h3 class="text-center emphasis">Get back the money you deserve</h3>
+				<h3 class="text-center emphasis accent">Get back the money you deserve</h3>
 				<p>Our thorough approach earns you more money even with complicated returns that others won't touch</p>
 			</div><!--/ .colspan-three -->
 			<div class="featured-el colspan-three">
 				<h2 class="bold text-center">Quickbooks</h2>
-				<h3 class="text-center emphasis">Talk to Our Pros</h3>
+				<h3 class="text-center emphasis accent">Talk to Our Pros</h3>
 				<p>We know Quickbooks inside and out. We can help you become a pro too. Find out more</p>
 			</div><!--/ .colspan-three -->						
 		</div><!--/ .featured -->
