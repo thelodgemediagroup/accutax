569300791d600db1ab1ae66dae6f5afb3dbd07a7
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-11-04 13:31:00 -0500

    Homepage link fix

--- dev/application/views/home.php
+++ dev/application/views/home.php
@@ -17,7 +17,7 @@
 				<h3 class="text-center emphasis accent">Get back the money you deserve</h3>
 				<p>Our thorough approach earns you more money even with complicated returns that others won't touch</p>
 				<div class="info-button">
-					<a href="<?php echo base_url('business/business'); ?>">Find Out More</a>
+					<a href="<?php echo base_url('personal/personal'); ?>">Find Out More</a>
 				</div><!--/ .info-button -->				
 			</div><!--/ .colspan-three -->
 			<div class="featured-el colspan-three">
@@ -25,7 +25,7 @@
 				<h3 class="text-center emphasis accent">Talk to Our Pros</h3>
 				<p>We know Quickbooks inside and out. We can help you become a pro too. Find out more</p>
 				<div class="info-button">
-					<a href="<?php echo base_url('business/business'); ?>">Learn</a>
+					<a href="<?php echo base_url('business/quickbooks'); ?>">Learn</a>
 				</div><!--/ .info-button -->				
 			</div><!--/ .colspan-three -->						
 		</div><!--/ .featured -->
