83c29eadfe1a444d1c2c7e7a7af9bfedb36a6f2f
Jesse Wozniak <wozniak.jesse@gmail.com>
2013-12-31 18:08:38 -0500

    Typo fix

--- dev/application/views/personal/quickbooks.php
+++ dev/application/views/personal/quickbooks.php
@@ -1,8 +1,8 @@
 <div class="subpage container clearfix">
 	<div class="page-content floatright category">
 		<h1>QuickBooks</h1>		
-		<h3 class="accent emphasis">Manage your personal finances like a pro.</h3>
-		<p>We can help you master the tools of the trade. While we are able to get you current with any QuickBooks files that you have, we also provide instruction to help you setup QuickBooks, keep track of your cash flow and get you prepared for tax season. Bring your files and learn to manage your finances like a pro.</p>
+		<h3 class="accent emphasis">Manage your personal finances like a professional.</h3>
+		<p>We can help you master the tools of the trade. While we are able to get you current with any QuickBooks files that you have, we also provide instruction to help you setup QuickBooks, keep track of your cash flow and get you prepared for tax season. Bring your files and learn to manage your finances like a professional.</p>
 	</div><!--/ .page-content -->
 
 	<?php echo $sidebar; ?>
