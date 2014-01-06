50690f7d7d5c424b739a8898bf45344265e6f526
Jesse Wozniak <wozniak.jesse@gmail.com>
2014-01-05 22:36:23 -0500

    Featured-el homepage all sizes

--- dev/application/views/home.php
+++ dev/application/views/home.php
@@ -8,7 +8,7 @@
 		<div class="featured container clear">
 			<div class="featured-el colspan-three">
 				<h2 class="semi-bold text-center">Personal</h2>
-				<h3 class="text-center emphasis accent featured-middle">Get Back the Money You Deserve</h3>
+				<h3 class="text-center emphasis accent featured-middle">Get the Money You Deserve</h3>
 				<p class="align-left">Our thorough approach earns you more money even with complex returns that other accountants will not touch.</p>
 				<div class="info-button">
 					<a href="<?php echo base_url('personal/personal'); ?>">Find Out More</a>
@@ -17,7 +17,7 @@
 			<div class="featured-el colspan-three">
 				<h2 class="semi-bold text-center">Business</h2>
 				<h3 class="text-center emphasis accent"><span class="italic">Own</span> Your Business</h3>
-				<p class="featured-short align-left">Let us manage your finances. We offer Accounting, Income Tax Returns, Payroll, QuickBooks help.</p>
+				<p class="align-left">Let us manage your finances. We offer Accounting, Income Tax Returns, Payroll, QuickBooks help.</p>
 				<div class="info-button">
 					<a href="<?php echo base_url('business/business'); ?>">Manage</a>
 				</div><!--/ .info-button -->
@@ -25,7 +25,7 @@
 			<div class="featured-el colspan-three">
 				<h2 class="semi-bold text-center">QuickBooks</h2>
 				<h3 class="text-center emphasis accent">Talk to Our Professionals</h3>
-				<p class="featured-short">We know QuickBooks inside and out. We can help you become a professional too. Find out more.</p>
+				<p>We know QuickBooks inside and out. We can help you become a professional too. Find out more.</p>
 				<div class="info-button">
 					<a href="<?php echo base_url('business/quickbooks'); ?>">Learn</a>
 				</div><!--/ .info-button -->				
